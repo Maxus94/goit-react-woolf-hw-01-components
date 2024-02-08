7f2031663e0f8967e3acf2795dc6e5b0d6e38b15
Maxim <kmaxusk@gmail.com>
2024-02-08 18:12:15 +0200

    ready-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,20 @@ import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
 import { TransactionHistory } from './Transactions/TransactionHistory';
-import user from '../user.json';
-import data from '../data.json';
-import friends from '../friends.json';
-import transactions from '../transactions.json';
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
 
-const divStyle = {
-  height: '100vh',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  fontSize: 40,
-  color: '#010101',
-};
+// const divStyle = {
+//   height: '100vh',
+//   display: 'flex',
+//   justifyContent: 'center',
+//   alignItems: 'center',
+//   fontSize: 40,
+//   color: '#010101',
+// };
 
 export const App = () => {
   return (
